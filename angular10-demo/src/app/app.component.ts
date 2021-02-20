6cdcfe4f4d492a688701e0022fdd137417e19227
weiyan.Lu <weiyan.Lu@cn.bosch.com>
2021-02-20 09:40:17 +0800

    Angular 10 tutorial #5 Interpolation

--- angular10-demo/src/app/app.component.ts
+++ angular10-demo/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   appTitle = 'angular 10 tutorial';
+  appDescription = 'Was this post helpful for you about the angular?';
+  backgroundImgUrl = 'assets/imgs/test.png';
+
+  getName(): string {
+    return 'razr';
+  }
+
+  fruits = [
+    { id: 1, name: 'Apple' },
+    { id: 2, name: 'Orange' },
+    { id: 3, name: 'Strawberry' }
+  ]
+
+  isDisabled: boolean = true;
 }
